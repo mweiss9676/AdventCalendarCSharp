90fb7847c54b2c144f3432153913208d81e0339b
Michael Weiss <mweis9676@gmail.com>
2018-02-04 12:44:18 -0500

    Day 20 parts 1 and 2

--- Day19.cs
+++ Day19.cs
@@ -9,10 +9,10 @@ namespace AdventCalendar2017
 {
     public class Day19
     {
-        static int totalSteps = 0;
-        static int lineCount;
+        static int totalSteps = 0;//keeps track of the total "steps" taken in the path      
+        static int lineCount;//how "tall" the file is
         static char[][] input;
-        static List<char> pathTaken = new List<char>();
+        static List<char> pathTaken = new List<char>();//a list holding the order in which Letters are encountered on the optimal path
         static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
         static void Main(string[] args)
@@ -21,7 +21,7 @@ namespace AdventCalendar2017
             StreamReader sr = new StreamReader(@"C:\Users\Michael Weiss\Documents\repo-ster\Advent_of_Code_2017\AdventCalendarCSharp\inputs\Day19.txt");
             lineCount = File.ReadLines(@"C:\Users\Michael Weiss\Documents\repo-ster\Advent_of_Code_2017\AdventCalendarCSharp\inputs\Day19.txt").Count();
 
-
+            //read each line into the char[][] input
             input = new char[lineCount][];
 
             for (int i = 0; i < input.Length; i++)
@@ -29,6 +29,9 @@ namespace AdventCalendar2017
                 input[i] = sr.ReadLine().ToCharArray();
             }
 
+
+            //the puzzle gurantees that the entry point will be at the top so this scans the puzzle
+            //starting at the top for the entry point which will be "|" and initiates a Down method on that char
             for (int y = 0; y < lineCount; y++)
             {
                 for (int x = 0; x < input[y].Length; x++)
@@ -42,6 +45,12 @@ namespace AdventCalendar2017
             }
         }
 
+        //Four methods to handle the y and x coordinates of the current position in the map
+        //each method continues along collecting any letters that show up and checking for the final letter of the puzzle
+        //when a "+" is encountered we pass along to the NewDirection method which takes the y and x coordinates and the direction
+        //that passed those coordinates to it in the form of a char. This is so we don't turn around an go backwards while deciding 
+        //a new direction. (note: only one space surround the "+" along the opposite axis from the one passed in
+        //will have a viable direction to travel in. That's why this works)
         static void Down(int y, int x)
         {
             while (input[y][x] != '+')
@@ -110,14 +119,22 @@ namespace AdventCalendar2017
             }
             NewDirection(y, x, 'r');
         }
+
+        //We decide what direction to go in. The puzzle eliminates the possibility of travelling along the same axis
+        //as a "+" always indicates a corner and only has one viable (within the index of the array) direction to travel in.
         static void NewDirection(int y, int x, char lastDirection)
         {
-            totalSteps++;
+            totalSteps++;//a plus sign is still a step and must be counted
+
+            //Determine whether going in any direction will throw an out of bounds exception
             bool canGoDown = y + 1 < input.Length;
             bool canGoUp = y - 1 >= 0;
             bool canGoLeft = x - 1 >= 0;
             bool canGoRight = x + 1 < input[y].Length;
 
+
+            //figure out the new direction based on eliminating the old one and it's opposite
+            //only one viable direction is available to call, so whichever direction contains a non-space char is that direction
             if (lastDirection == 'd' || lastDirection == 'u')
             {
                 if (canGoLeft && input[y][x - 1] != ' ')
@@ -143,6 +160,8 @@ namespace AdventCalendar2017
  
         }
 
+        //called only when the final letter is found. Ends the program and prints out the total number of steps taken and the 
+        //full path of letters in the order they were encountered. 
         static void PrintPath()
         {
             totalSteps++;
