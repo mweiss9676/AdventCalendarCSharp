105870782317fab74c01118aced88ebe499d3cf2
Michael Weiss <mweis9676@gmail.com>
2018-02-21 09:34:44 -0500

    random commit

---
+++ asdfads.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCalendar2017
+{
+    public class asdfads
+    {
+        static Dictionary<string, string> someDict = new Dictionary<string, string>();
+
+        public static void Main(string[] args)
+        {
+            someDict["...#"] = "###..###.";
+
+            if (someDict.ContainsKey("...#"))
+            {
+                Console.WriteLine("it's there");
+            }
+            else
+            {
+                Console.WriteLine("it's not there");
+            }
+        }
+    }
+}
